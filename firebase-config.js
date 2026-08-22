e05b100edeba659dc24e655417eab968df4d72bb
farhanbhoraniya84-design <farhanbhoraniya84@gmail.com>
2026-08-22 09:23:50 +0530

    Update Firebase configuration details
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, push, set, onValue, update } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-// Apne Firebase Console se mila hua Config yahan replace karein
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD... (Aapki Firebase apiKey)",
+  authDomain: "zayko-app-xxxx.firebaseapp.com",
+  databaseURL: "https://zayko-app-xxxx-default-rtdb.firebaseio.com",
+  projectId: "zayko-app-xxxx",
+  storageBucket: "zayko-app-xxxx.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef123456"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app);
-
-export { db, ref, push, set, onValue, update, auth, signInWithEmailAndPassword };
